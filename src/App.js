36ee533b385db2c495585888cc000d077dd643d9
Erik Stone <spologas@gmail.com>
2021-10-05 17:06:53 -0700

    following along web dev simplified learn react. at 19:37 and installed uuidv4 for Todo ids

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList'
+import uuidv4 from './uuidv4';
 function App() {
-  return null
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [... prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+  return (
+    <>
+    <TodoList todos = {todos} />
+    <input ref={todoNameRef} type="text" />
+    <button onCLick={handleAddTodo}>Add Todo</button>
+    <button> Clear Complete</button>
+    <button>0 left to do</button>
+    </>
+  )
 }
 
 export default App;
