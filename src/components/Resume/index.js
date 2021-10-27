d2e8ed132e4475b47faadbb8193da2e8ae25d5af
Erik Stone <spologas@gmail.com>
2021-10-26 17:14:24 -0700

    trying to add resume in pdf form, will have to try after work.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,24 +1,26 @@
 // The Resume section must have a downloadable resume and a list of developer's proficiencies.
-import React from "react";
-import { Document, Page } from "react-pdf";
+import React, {useState }from "react";
+import { Document } from "react-pdf";
 
-export default function ResumeApp(props) {
-
+export default function AllPages(props) {
     const [numPages, setNumPages] = useState(null);
-   
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
 
-    const {pdf} = props;
+    const { pdf } = props;
+    return (
+        <Document
+        file={pdf}
+        options={{ resumeSrc: "/pdf.Resume.pdf" }}
+        onLoadSuccess={onDocumentLoadSuccess}
+        >
 
-    return ( 
-        <div>
-            <Document file= "https://drive.google.com/file/d/1yPLEK35BKnUWy67JRonzp7ZBu5w1YCJK/view?usp=sharing" />
-               
-           
-           
-        </div>
+            {Array.from(new Array(numPages), (el, index) => (
+                <page key={'page_${index + 1}'} pageNumber={index + 1} />
+            ))}
+        </Document>
     );
+
 }
