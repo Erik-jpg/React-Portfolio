57f7d8b334dd7e1c93aafba6bc8e92f70f686938
Erik Stone <spologas@gmail.com>
2022-02-19 11:11:26 -0800

    added rendering for image.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
-
 import resumePdf from './Resume.pdf';
 import resumeImg from './ResumePicture.png';
+
+const Resume = (props) => (
+    <div className="box" style={{ background: 'black' }}>
+        <div className="columns">
+            <div className="column" style={{display: flex, justifyContent: 'center'}}>
+<figure> <a href={resumePdf} download><img id="ResumePicture" src={resumeImg} alt="ResumeSample" />
+</a></figure>
+        </div>
+        </div>
+        </div>
+);
