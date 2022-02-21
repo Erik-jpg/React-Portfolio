54da766f338f9e9ad7f72109acc5dd3051e56459
Erik Stone <spologas@gmail.com>
2022-02-21 10:38:55 -0800

    added App, index, and stylesheet.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
+import './stylesheet.css';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <footer />
+        </div>
+    );
+}
+
+export default App;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Document } from 'react-pdf';
+
 import resumePdf from './Resume.pdf';
 import resumeImg from './ResumePicture.png';
 
-<figure>
-    <a href={resumePdf}
-        download><img src={resumeImg} alt='Png of resume' /></a>
-</figure>
+const Resume = (props) => (
+    <div className="box" style={{ background: 'black' }}>
+        <div className="columns">
+            <div className="column" style={{display: flex, justifyContent: 'center'}}>
+<figure> <a href={resumePdf} download><img id="ResumePicture" src={resumeImg} alt="ResumeSample" />
+</a></figure>
+        </div>
+    </div>
+</div>
+);
 
-export default Document;
+export default Resume;
