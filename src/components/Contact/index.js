53e512bcd226083e5ab5941a3a32aea3511fb922
Erik Stone <spologas@gmail.com>
2022-03-07 09:59:28 -0800

    added contact form, saving before adding node server for email.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ContactForm from './ContactForm';
 
+const ContactMe = () => {
+    return (
+        <div className="contact">
+        <ContactForm />    
+        </div>
+    );
+}
+
+export default ContactMe;
