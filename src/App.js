db4c339c0a8bec1d9119fd1938be7c566176fa72
Erik Stone <spologas@gmail.com>
2022-04-11 12:34:41 -0700

    removed react-particles-js, needed to switch to yarn to use.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
-import ParticlesBackground from './ParticlesBackground';
 import './stylesheet.css';
 
 
@@ -9,7 +8,6 @@ function App() {
 return (
   <div>
     <Header />
-    <ParticlesBackground />
     <Footer />
   </div>
 );
